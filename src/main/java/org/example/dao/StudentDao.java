3a7de12aeccdb627e5b8841584bc4ff208ed63e1
Pratiksha Kute <pratikute1603@gmail.com>
2023-09-11 21:25:27 +0530

    Initial Commit

--- src/main/java/org/example/dao/StudentDao.java
+++ src/main/java/org/example/dao/StudentDao.java
@@ -1,8 +1,11 @@
 package org.example.dao;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
 import org.example.entities.Student;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public class StudentDao
 {
@@ -17,11 +20,29 @@ public class StudentDao
       //getting single data
       public Student getStudent(int id)
       {
-          Student student1=(Student)this.hibernateTemplate.get("Student.java",id);
+          Student student1=(Student)this.hibernateTemplate.get(Student.class,id);
           return student1;
       }
 
+//get all data
+    public List<Student> getAllData(int StudentId)
+    {
+        List<Student> list= (List<Student>) this.hibernateTemplate.loadAll(Student.class);
+        return list;
+    }
 
+    //delete data
+    public void delete(int id)
+    {
+        Student student=this.hibernateTemplate.get(Student.class,id);
+                this.hibernateTemplate.delete(id);
+    }
+
+    //update data
+    public  void update(int id)
+    {
+        this.hibernateTemplate.update(id);
+    }
 
     public HibernateTemplate getHibernateTemplate() {
 
