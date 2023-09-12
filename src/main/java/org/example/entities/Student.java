8ab2cb175f869ac906a5b01f9381e1b7d6e71b11
Pratiksha Kute <pratikute1603@gmail.com>
2023-09-12 08:39:34 +0530

    Initial Commit

--- src/main/java/org/example/entities/Student.java
+++ src/main/java/org/example/entities/Student.java
@@ -48,4 +48,13 @@ public class Student {
     public void setCity(String city) {
         this.city = city;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
